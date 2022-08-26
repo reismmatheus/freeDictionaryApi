2403c865259560b50fba80af83c519fd77a2e7a6
Matheus Reis <reis.mmatheus@gmail.com>
2022-08-26 01:11:12 -0300

    Creating the Service

---
+++ FreeDictionaryApi/FreeDictionary.Service/FreeDictionaryApi/FreeDictionaryApiClient.cs
@@ -0,0 +1,34 @@
+﻿using FreeDictionary.Service.FreeDictionaryApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FreeDictionary.Service.FreeDictionaryApi
+{
+    public class FreeDictionaryApiClient
+    {
+        private readonly HttpClient _client;
+        private readonly string _apiUrl;
+        private readonly string _urlFile;
+        public FreeDictionaryApiClient(HttpClient client)
+        {
+            _client = client;
+            _apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en";
+        }
+        public async Task<FreeDictionaryApiResult> Get(string word)
+        {
+            var response = await _client.GetAsync($"{_apiUrl}/{word}");
+            var result = JsonSerializer.Deserialize<List<FreeDictionaryApiResult>>(response.ToString());
+            return result.FirstOrDefault();
+        }
+        public async Task<List<string>> Download()
+        {
+            var response = await _client.GetAsync($"{_urlFile}}");
+            var result = JsonSerializer.Deserialize<List<string>>(response.ToString());
+            return result;
+        }
+    }
+}
---
+++ FreeDictionaryApi/FreeDictionary.Service/FreeDictionaryApi/Model/FreeDictionaryApiResult.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FreeDictionary.Service.FreeDictionaryApi.Model
+{
+    public class FreeDictionaryApiResult
+    {
+        [JsonPropertyName("word")]
+        public string Word;
+
+        [JsonPropertyName("phonetics")]
+        public List<PhoneticResult> Phonetics;
+
+        [JsonPropertyName("meanings")]
+        public List<MeaningResult> Meanings;
+
+        [JsonPropertyName("license")]
+        public LicenseResult License;
+
+        [JsonPropertyName("sourceUrls")]
+        public List<string> SourceUrls;
+
+        [JsonPropertyName("title")]
+        public string Title;
+
+        [JsonPropertyName("message")]
+        public string Message;
+
+        [JsonPropertyName("resolution")]
+        public string Resolution;
+    }
+
+    public class DefinitionResult
+    {
+        [JsonPropertyName("definition")]
+        public string Definition;
+
+        [JsonPropertyName("synonyms")]
+        public List<object> Synonyms;
+
+        [JsonPropertyName("antonyms")]
+        public List<object> Antonyms;
+
+        [JsonPropertyName("example")]
+        public string Example;
+    }
+
+    public class LicenseResult
+    {
+        [JsonPropertyName("name")]
+        public string Name;
+
+        [JsonPropertyName("url")]
+        public string Url;
+    }
+
+    public class MeaningResult
+    {
+        [JsonPropertyName("partOfSpeech")]
+        public string PartOfSpeech;
+
+        [JsonPropertyName("definitions")]
+        public List<DefinitionResult> Definitions;
+
+        [JsonPropertyName("synonyms")]
+        public List<string> Synonyms;
+
+        [JsonPropertyName("antonyms")]
+        public List<string> Antonyms;
+    }
+
+    public class PhoneticResult
+    {
+        [JsonPropertyName("audio")]
+        public string Audio;
+
+        [JsonPropertyName("sourceUrl")]
+        public string SourceUrl;
+
+        [JsonPropertyName("license")]
+        public License License;
+
+        [JsonPropertyName("text")]
+        public string Text;
+    }
+}
